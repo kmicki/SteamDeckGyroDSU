80925344a21c789f9a6076b94838e32272b4dc33
kmicki <kmicki@kmicki.km>
2022-05-03 22:15:30 +0200

    reafactored hiddevfinder out to a file

--- inc/hiddev/hiddevfinder.h
+++ inc/hiddev/hiddevfinder.h
@@ -1,4 +1,12 @@
 #ifndef _KMICKI_HIDDEV_HIDDEVFINDER_H_
 #define _KMICKI_HIDDEV_HIDDEVFINDER_H_
 
+#include <cstdint>
+
+namespace kmicki::hiddev
+{
+    // find which X among /dev/usb/hiddevX fits provided VID+PID
+    int FindHidDevNo(uint16_t vid, uint16_t pid);
+}
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "hiddevreader.h"
+#include "hiddevfinder.h"
 #include "sdhidframe.h"
 #include "shell.h"
 #include <ncurses.h>
@@ -6,86 +7,58 @@
 #include <sstream>
 #include <iomanip>
 
-
 using namespace kmicki::shell;
 using namespace kmicki::sdgyrodsu;
 using namespace kmicki::hiddev;
 
-    #define FRAME_LEN 64
-    #define FRAMECNT_PER_FILE 5000
-    #define SCAN_PERIOD_MS 2
+#define FRAME_LEN 64
+#define FRAMECNT_PER_FILE 5000
+#define SCAN_PERIOD_MS 2
 
-    typedef HidDevReader::frame_t frame_t;
+#define VID 0x28de
+#define PID 0x1205
 
-    // find which X among /dev/usb/hiddevX fits Steam Deck Controls
-    int findHidDevNo() 
-    {
-        std::string output;
-        int bus,dev,bus2,dev2;
-        int testlen;
-        // Steam Deck controls: usb device VID: 28de, PID: 1205
-        if(ExecuteCommand("lsusb | grep '28de:1205' | sed -e \"s/.*Bus \\([0-9]\\+\\) Device \\([0-9]\\+\\).*/\\1 \\2/g\"",output) || (testlen = output.length()) != 8)
-            return -1;
+typedef HidDevReader::frame_t frame_t;
 
-        std::istringstream str(output);
-        str >> bus >> dev;
-        if (bus == 0 || dev == 0)
-            return -1;
+void drawData(frame_t frame,int num)
+{
+    static int maxSpan = 0;
+    static uint32_t lastInc;
+    uint32_t newInc = *(reinterpret_cast<uint32_t *>(frame.data()+4));
 
-        for (int i = 0; i < 10; i++)    
-        {
-            std::ostringstream ostr;
-            ostr << "udevadm info --query=all /dev/usb/hiddev" << i << " | grep 'P:' | sed -e \"s/.*usb\\([0-9]\\+\\).*\\.\\([0-9]\\+\\).*/\\1 \\2/g\"";
-            std::string cmd = ostr.str();
-            if(ExecuteCommand(cmd,output) || output.length() > 8 ||  output.length() < 4)
-                continue;
-            std::istringstream str(output);
-            str >> bus2 >> dev2;
-            if (bus == bus2 || dev == dev2)
-                return i;
-        }
-        return -1;
+    if(lastInc && newInc-lastInc > maxSpan)
+        maxSpan = newInc-lastInc;
+
+    move(0,0);
+    printw("%5d %d %d             ",num,newInc-lastInc,maxSpan);
+    move(2,0);
+    int k = 2;
+    for (int i = 0; i < FRAME_LEN; ++i) {
+        printw("%.2x ", (unsigned char) frame[i]);
+        if(i % 16 == 15)
+            move(++k,0);
     }
 
-    void drawData(frame_t frame,int num)
-    {
-        static int maxSpan = 0;
-        static uint32_t lastInc;
-        uint32_t newInc = *(reinterpret_cast<uint32_t *>(frame.data()+4));
+    SdHidFrame* sd = reinterpret_cast<SdHidFrame*>(frame.data());
 
-        if(lastInc && newInc-lastInc > maxSpan)
-            maxSpan = newInc-lastInc;
+    ++k;
+    move(++k,0); printw("U1: %d         ",sd->Unknown1);
+    move(++k,0); printw("U2: %d         ",sd->Unknown2);
+    move(++k,0); printw("U3: %d         ",sd->Unknown3);
+    move(++k,0); printw("U4: %d         ",sd->Unknown4);
+    ++k;
+    move(++k,0); printw("A_RL: %d         ",sd->AccelAxisRightToLeft);
+    move(++k,0); printw("A_TB: %d         ",sd->AccelAxisTopToBottom);
+    move(++k,0); printw("A_FB: %d         ",sd->AccelAxisFrontToBack);
+    ++k;
+    move(++k,0); printw("G_RL: %d         ",sd->GyroAxisRightToLeft);
+    move(++k,0); printw("G_TB: %d         ",sd->GyroAxisTopToBottom);
+    move(++k,0); printw("G_FB: %d         ",sd->GyroAxisFrontToBack);
 
-        move(0,0);
-        printw("%5d %d %d             ",num,newInc-lastInc,maxSpan);
-        move(2,0);
-        int k = 2;
-        for (int i = 0; i < FRAME_LEN; ++i) {
-            printw("%.2x ", (unsigned char) frame[i]);
-            if(i % 16 == 15)
-                move(++k,0);
-        }
+    refresh();
 
-        SdHidFrame* sd = reinterpret_cast<SdHidFrame*>(frame.data());
-
-        ++k;
-        move(++k,0); printw("U1: %d         ",sd->Unknown1);
-        move(++k,0); printw("U2: %d         ",sd->Unknown2);
-        move(++k,0); printw("U3: %d         ",sd->Unknown3);
-        move(++k,0); printw("U4: %d         ",sd->Unknown4);
-        ++k;
-        move(++k,0); printw("A_RL: %d         ",sd->AccelAxisRightToLeft);
-        move(++k,0); printw("A_TB: %d         ",sd->AccelAxisTopToBottom);
-        move(++k,0); printw("A_FB: %d         ",sd->AccelAxisFrontToBack);
-        ++k;
-        move(++k,0); printw("G_RL: %d         ",sd->GyroAxisRightToLeft);
-        move(++k,0); printw("G_TB: %d         ",sd->GyroAxisTopToBottom);
-        move(++k,0); printw("G_FB: %d         ",sd->GyroAxisFrontToBack);
-
-        refresh();
-
-        lastInc = newInc;
-    }
+    lastInc = newInc;
+}
 
 using namespace kmicki::sdgyrodsu;
 
@@ -93,7 +66,8 @@ static std::exception_ptr teptr = nullptr;
 
 int main()
 {
-    int hidno = findHidDevNo();
+    // Steam Deck controls: usb device VID: 28de, PID: 1205
+    int hidno = FindHidDevNo(VID,PID);
     if(hidno < 0) 
     {
         std::cerr << "Device not found." << std::endl;
