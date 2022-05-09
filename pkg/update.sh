0f81d7d5db057d98f6763948599b067f8fd070c0
kmicki <1463619+kmicki@users.noreply.github.com>
2022-05-09 10:01:55 +0200

    Modified update script to run install script
    Modified udpate script to run install script after downloading new release.
    Fixed the issue that update script worked properly only when put in a specific directory.

--- pkg/update.sh
+++ pkg/update.sh
@@ -1,16 +1,19 @@
-@echo off
-echo "Grabbing latest .zip file..."
-curl -L -O https://github.com/kmicki/SteamDeckGyroDSU/releases/latest/download/SteamDeckGyroDSUSetup.zip
-unzip -o SteamDeckGyroDSUSetup.zip -d /home/deck
-echo "Stopping service..."
-systemctl --user stop sdgyrodsu.service
-systemctl --user disable sdgyrodsu.service
-echo "Copying files..."
-rm $HOME/sdgyrodsu/sdgyrodsu
-cp sdgyrodsu $HOME/sdgyrodsu/
-chmod +x $HOME/sdgyrodsu/sdgyrodsu
-echo "Installing service..."
-rm $HOME/.config/systemd/user/sdgyrodsu.service
-cp sdgyrodsu.service $HOME/.config/systemd/user/
-systemctl --user enable --now sdgyrodsu.service
-echo "Installation done."
+echo "Grabbing latest release..."
+if curl -L -O -s https://github.com/kmicki/SteamDeckGyroDSU/releases/latest/download/SteamDeckGyroDSUSetup.zip >/dev/null; then
+	echo "Latest release downloaded."
+else
+	echo -e "\e[1mFailed to grab latest .zip file...\e[0m"
+	exit 10
+fi
+echo "Extracting files..."
+if unzip -o SteamDeckGyroDSUSetup.zip -d $HOME >/dev/null; then
+	echo "Files extracted."
+else
+	echo -e "\e[1mFailed to extract files from downloaded .zip release...\e[0m"
+	exit 11
+fi
+cd $HOME/SteamDeckGyroDSUSetup
+echo "Running install script..."
+echo " "
+./install.sh
+exit $?
