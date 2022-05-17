261ea1b112c4ae8de6f7fd0887382761fb6a5f98
kmicki <1463619+kmicki@users.noreply.github.com>
2022-05-17 16:03:07 +0200

    Conditional installing of dependencies in build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,69 @@
-sudo steamos-readonly disable
-sudo pacman-key --init
-sudo pacman-key --populate
-sudo pacman -S --noconfirm gcc
-sudo pacman -S --noconfirm glibc
-sudo pacman -S --noconfirm linux-api-headers
-sudo pacman -S --noconfirm ncurses
-sudo steamos-readonly enable
+# Required packages
+declare -a dependencies=("gcc" "glibc" "linux-api-headers" "ncurses")
 
-mkdir -p bin
+# Some header file for each package.
+# SteamOS has all required packages installed by default but for some reason header files are missing.
+# This will be checked before building.
+declare -a checks=(
+    "/usr/include/c++/11.1.0/vector"
+    "/usr/include/errno.h"
+    "/usr/include/linux/can/error.h"
+    "/usr/include/ncurses.h"
+)
+declare -a reinstall
+common_reinstall=false
 
-rm bin/*
+for i in ${!checks[@]}; do
+    if [ ! -f "${checks[$i]}" ]; then
+        # header file is missing
+	reinstall[$i]=true
+        common_reinstall=true
+    else
+        reinstall[$i]=false
+    fi
+done
 
+if [ "$common_reinstall" == true ]; then
+    echo "Some dependencies' header files missing."
+    # Check if read only filesystem is enabled
+    rofs=false
+    if sudo steamos-readonly status | grep -q 'enabled'; then
+        rofs=true
+	echo "Read only filesystem enabled. Disabling..."
+	sudo steamos-readonly disable &>/dev/null
+    fi
+    echo "Initializing package manager..."
+    sudo pacman-key --init &>/dev/null
+    sudo pacman-key --populate &>/dev/null
+    for i in ${!reinstall[@]}; do
+        if [ "${reinstall[$i]}" == true ]; then
+            echo -e "Reinstalling \e[1m${dependencies[$i]}\e[0m..."
+            sudo pacman -S --noconfirm ${dependencies[$i]} &>/dev/null
+        fi
+    done
+    if [ "$rofs" == true ]; then
+        echo "Reenabling read only filesystem..."     
+	sudo steamos-readonly disable &>/dev/null
+    fi
+    echo "Required dependencies reinstalled."
+else
+    echo "All dependencies' header files installed."
+fi
+
+echo -e "Create \e[1mbin\e[0m directory..."
+mkdir -p bin >/dev/null
+
+echo -e "Clearing \e[1mbin\e[0m directory..."
+rm bin/* &>/dev/null
+
+echo "Building source..."
 g++ $(find inc -type d -printf '-I %p\n') -g $(find src -type f -iregex '.*\.cpp' -printf '%p\n') -pthread -lncurses -o bin/sdgyrodsu
+
+result=$?
+
+if [ "$result" -ne 0 ]; then
+    echo -e "\e[1mError: source was not built.\e[0m"
+    exit $result
+else
+    echo -e "\e[1mBuilding complete.\e[0m"
+fi
