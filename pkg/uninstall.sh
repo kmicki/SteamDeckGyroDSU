47cf95af4a71d9075369fe68aa07648bbe76ea5c
kmicki <1463619+kmicki@users.noreply.github.com>
2022-06-03 14:30:42 +0200

    scripts updated

--- pkg/uninstall.sh
+++ pkg/uninstall.sh
@@ -3,8 +3,11 @@ systemctl --user -q stop sdgyrodsu.service >/dev/null 2>&1
 systemctl --user -q disable sdgyrodsu.service >/dev/null 2>&1
 rm $HOME/.config/systemd/user/sdgyrodsu.service >/dev/null 2>&1
 
-echo "Removing binary"
+echo "Removing files"
 rm $HOME/sdgyrodsu/sdgyrodsu >/dev/null 2>&1
+rm $HOME/sdgyrodsu/update.sh >/dev/null 2>&1
+rm $HOME/sdgyrodsu/uninstall.sh >/dev/null 2>&1
+rm $HOME/sdgyrodsu/logcurrentrun.sh >/dev/null 2>&1
 rm -d $HOME/sdgyrodsu >/dev/null 2>&1
 
 echo "Removing USB permissions"
